34623180b9523ddc34c3ce08c0cdfc48edc7e3b4
Lee Henson <lee.m.henson@gmail.com>
2011-02-15 14:08:00 +0000

    Added commit tracker specs

---
+++ spec/event_store/persistence/commit_tracker_spec.rb
@@ -0,0 +1,31 @@
+require_relative '../../spec_helper'
+
+describe ::EventStore do
+  describe 'commit tracker' do
+    let(:max_commits_to_track_per_stream) { 2 }
+    let(:tracker) { EventStore::CommitTracker.new max_commits_to_track_per_stream }
+    let(:stream_id) { UUID.new }
+
+    let(:tracked_commits) { [ commit, commit, commit ] }
+    let(:untracked) { commit(:stream_id => '', :commit_id => tracked_commits.first.commit_id) }
+    let(:still_tracked) { commit(:stream_id => tracked_commits.last.stream_id, :commit_id => tracked_commits.last.commit_id) }
+    let(:dropped_from_tracking) { commit(:stream_id => tracked_commits.first.stream_id, :commit_id => tracked_commits.first.commit_id) }
+
+    context 'when tracking commits' do
+      before { tracked_commits.each { |c| tracker.track c } }
+
+      it('only contains stream explicitly tracked') { tracker.contains?(untracked).should be_false }
+      it('finds tracked commits') { tracker.contains?(still_tracked).should be_true }
+      it('only tracks the specified number of commits') { tracker.contains?(dropped_from_tracking).should be_false }
+    end
+
+    def commit(options = {})
+      defaults = { :stream_id => stream_id,
+                   :stream_revision => 0,
+                   :commit_id => UUID.new,
+                   :commit_sequence => 0 }
+
+      EventStore::Commit.new(defaults.merge options)
+    end
+  end
+end
