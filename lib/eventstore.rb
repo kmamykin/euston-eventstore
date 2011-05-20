cdcc012e7b7a6068cfcf3bb5dd01b1773b297fc9
Lee Henson <lee.m.henson@gmail.com>
2011-05-20 17:18:04 +0100

    Serializer no longer exists in this gem

--- lib/eventstore.rb
+++ lib/eventstore.rb
@@ -23,7 +23,6 @@ require 'eventstore/persistence/mongodb/mongo_persistence_engine'
 require 'eventstore/persistence/mongodb/mongo_persistence_factory'
 require 'eventstore/persistence/mongodb/mongo_snapshot'
 require 'eventstore/persistence/mongodb/mongo_stream_head'
-require 'eventstore/serialization/mongodb/mongo_serializer'
 
 module EventStore
   FIXNUM_MAX = (2**(0.size * 8 -2) -1)
