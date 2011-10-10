a65b03138311a384786b0d3ea7474264a81d3790
Lee Henson <lee.m.henson@gmail.com>
2011-10-10 19:21:11 +0100

    uncommitted commands are not hashes

--- lib/euston-eventstore/repository.rb
+++ lib/euston-eventstore/repository.rb
@@ -11,7 +11,7 @@ module Euston
       def save aggregate
         stream = event_store.open_stream :stream_id => aggregate.aggregate_id
         aggregate.uncommitted_events.each { |e| stream << EventStore::EventMessage.new(e.to_hash.stringify__keys) }
-        aggregate.uncommitted_commands.each { |c| stream << EventStore::CommandMessage.new(c.stringify__keys) }
+        aggregate.uncommitted_commands.each { |c| stream << EventStore::CommandMessage.new(c.to_hash.stringify__keys) }
         stream.uncommitted_headers[:aggregate_type] = aggregate.class.to_s
         stream.commit_changes Euston.uuid.generate
       end
