b9f88507fb54325deb214a25e69169e50c64bfdc
Lee Henson <lee.m.henson@gmail.com>
2012-02-02 18:51:15 +0000

    revert back to persistence-agnostic event/command classes

--- lib/euston-eventstore/repository.rb
+++ lib/euston-eventstore/repository.rb
@@ -10,8 +10,8 @@ module Euston
 
       def save aggregate
         stream = aggregate.stream
-        aggregate.uncommitted_events.each { |e| stream << EventStore::Persistence::Mongodb::MongoEventMessage.new(e.to_hash.stringify__keys) }
-        aggregate.uncommitted_commands.each { |c| stream << EventStore::Persistence::Mongodb::MongoCommandMessage.new(c.to_hash.stringify__keys) }
+        aggregate.uncommitted_events.each { |e| stream << EventStore::EventMessage.new(e.to_hash.stringify__keys) }
+        aggregate.uncommitted_commands.each { |c| stream << EventStore::CommandMessage.new(c.to_hash.stringify__keys) }
         stream.uncommitted_headers[:aggregate_type] = aggregate.class.to_s
         stream.commit_changes Euston.uuid.generate
       end
