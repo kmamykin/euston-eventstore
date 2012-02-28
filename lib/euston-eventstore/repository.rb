dfef48f9dccc4f311c0602b66e676f1f861ab3b2
Lee Henson <lee.m.henson@gmail.com>
2012-02-28 09:51:10 +0000

    make sure all uncommitted headers are persisted

--- lib/euston-eventstore/repository.rb
+++ lib/euston-eventstore/repository.rb
@@ -12,6 +12,7 @@ module Euston
         stream = aggregate.stream || event_store.create_stream(aggregate.aggregate_id)
         aggregate.uncommitted_events.each { |e| stream << EventStore::EventMessage.new(e.to_hash.stringify__keys) }
         aggregate.uncommitted_commands.each { |c| stream << EventStore::CommandMessage.new(c.to_hash.stringify__keys) }
+        aggregate.uncommitted_headers.each { |key, value| stream.uncommitted_headers[key] = value }
         stream.uncommitted_headers[:aggregate_type] = aggregate.class.to_s
         stream.commit_changes Euston.uuid.generate
       end
